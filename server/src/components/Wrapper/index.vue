01156dd3cb37cf484f3d8b2d179b97a63e59582a
Takayyz <like.the.wind0306@gmail.com>
2020-03-16 09:48:40 +0900

    add: add wrapper component

---
+++ server/src/components/Wrapper/index.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="wrapper">
+    <div class="inner">
+      <main class="main">
+        <h1 class="title">Search book</h1>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Header from 'components/Header';
+// import Footer from 'components/Footer';
+
+export default {
+  // components: {
+  //   appHeader: Header,
+  //   appFooter: Footer,
+  // }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  width: 800px;
+}
+</style>
