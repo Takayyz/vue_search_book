a828baaf3e319890819f53fa2acdbb8afa14f4a8
Takayyz <like.the.wind0306@gmail.com>
2020-03-17 09:12:23 +0900

    update: HelloWorld.vueを書籍検索用テンプレに変更

--- server/src/components/HelloWorld.vue
+++ server/src/components/HelloWorld.vue
@@ -1,85 +1,112 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <div class="wrap">
+      <div class="container">
+        <div class="header">
+          <p class="header__title">Search Books! by Vue.js</p>
+        </div>
+        <div class="search">
+          <div class="search__text">
+            <input type="text" id="js-search-word" class="search__text__input" placeholder="タイトル、著者...">
+          </div>
+          <button id="js-search-button" class="search__btn">検索する</button>
+        </div>
+        <ul class="lists">
+          <li class='lists__item'>
+          <div class='lists__item__inner'>
+            <a href='' class='lists__item__link' target='_blank'>
+              <img src='' class='lists__item__img' alt=''>
+              <p class='lists__item__detail'>作品名：</p>
+              <p class='lists__item__detail'>作者　：</p>
+              <p class='lists__item__detail'>出版社：</p>
+            </a>
+          </div>
+         </li>
+        </ul>
+      </div>
+    </div>
+    <!-- <div class="hello">
+      <h1>{{ msg }}</h1>
+      <h2>Essential Links</h2>
+      <ul>
+        <li>
+          <a
+            href="https://vuejs.org"
+            target="_blank"
+          >
+            Core Docs
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://forum.vuejs.org"
+            target="_blank"
+          >
+            Forum
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://chat.vuejs.org"
+            target="_blank"
+          >
+            Community Chat
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/vuejs"
+            target="_blank"
+          >
+            Twitter
+          </a>
+        </li>
+        <br>
+        <li>
+          <a
+            href="http://vuejs-templates.github.io/webpack/"
+            target="_blank"
+          >
+            Docs for This Template
+          </a>
+        </li>
+      </ul>
+      <h2>Ecosystem</h2>
+      <ul>
+        <li>
+          <a
+            href="http://router.vuejs.org/"
+            target="_blank"
+          >
+            vue-router
+          </a>
+        </li>
+        <li>
+          <a
+            href="http://vuex.vuejs.org/"
+            target="_blank"
+          >
+            vuex
+          </a>
+        </li>
+        <li>
+          <a
+            href="http://vue-loader.vuejs.org/"
+            target="_blank"
+          >
+            vue-loader
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/vuejs/awesome-vue"
+            target="_blank"
+          >
+            awesome-vue
+          </a>
+        </li>
+      </ul>
+    </div> -->
   </div>
 </template>
 
@@ -96,6 +123,107 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.header {
+  width: 100%;
+  background-color: #43cee0;
+}
+.header__title {
+  line-height: 60px;
+  text-align: center;
+  font-size: 20px;
+  color: #fff;
+}
+.search {
+  overflow: hidden;
+  margin-bottom: 50px;
+  background: #fff;
+  box-shadow: 0 1px 5px #ccc;
+}
+.search__text {
+  width: 100%;
+}
+.search__text__input {
+  -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+  width: 100%;
+  margin-bottom: 20px;
+  padding: 0 10px;
+  line-height: 3em;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  border-bottom: 1px solid #eee;
+  font-size: 20px;
+  outline: none;
+}
+.search__btn {
+  display: block;
+  margin: 0 auto 20px;
+  padding: 0 20px;
+  line-height: 40px;
+  border: none;
+  font-size: 18px;
+  color: #fff;
+  background-color: #43cee0;
+  cursor: pointer;
+}
+.search__btn:hover {
+  background-color: #1eabbd;
+}
+.lists {
+  overflow: hidden;
+  margin-left: 20px;
+}
+.lists__item {
+  -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+  display: inline-block;
+  width: 25%;
+  padding: 0 20px 20px 0;
+  vertical-align: top;
+  text-align: center;
+}
+.lists__item__inner {
+  width: 100%;
+  max-width: 200px;
+  margin: 0 auto;
+}
+.lists__item__link {
+  display: block;
+  text-decoration: none;
+}
+.lists__item__img {
+  margin-bottom: 20px;
+  width: 100%;
+  max-width: 150px;
+  -webkit-box-shadow: 0 1px 5px #ccc;
+      box-shadow: 0 1px 5px #ccc;
+}
+.lists__item__detail {
+  margin-bottom: 10px;
+  padding-left: 5em;
+  text-indent: -5em;
+  line-height: 1.5em;
+  text-align: left;
+  font-size: 12px;
+}
+.message {
+  margin-bottom: 50px;
+  text-align: center;
+}
+
+@media screen and (max-width: 767px) {
+  .lists__item {
+    width: 33.33%;
+  }
+}
+
+@media screen and (max-width: 479px) {
+  .lists__item {
+    width: 50%;
+  }
+}
+
 h1, h2 {
   font-weight: normal;
 }
